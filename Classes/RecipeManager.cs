d7e47d30079a34f114f746a5775e90dc5aed079c
mobtone <tonenordstrom@hotmail.com>
2024-10-16 13:30:33 +0200

    New class: RecipeManager

---
+++ Classes/RecipeManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace GrupparbeteFoodapplication.Classes
+{
+    public class RecipeManager
+    {
+        private List<Recipe> recipes = new List<Recipe>();
+
+        public void AddRecipe(Recipe recipe)
+        {
+            recipes.Add(recipe);
+        }
+
+        public void UpdateRecipe(RecipeManager updatedRecipe)
+        {
+            var 
+        }
+    }
+}
