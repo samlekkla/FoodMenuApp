2b91cebead9097f14052f5c53fa0cc67efc7a2eb
ebru mermer <ebrumermer@live.se>
2024-10-16 11:02:26 +0200

    test konflikt

--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,12 @@ namespace GrupparbeteFoodapplication
         private void buttonTest_Click(object sender, EventArgs e)
         {
             string text = "Hello, world!";
-            int tal1 = 1;
-            int tal2 = 2;
+            int tal'5' = 1;
+            int tal7 = 2;
             int result = tal1 + tal2;
 
 
-            MessageBox.Show($"{text}\n{result}");
+           // MessageBox.Show($"{text}\n{result}");
         }
     }
 }
