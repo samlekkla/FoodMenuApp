b2dbdcbcc8bf05423c8e7ae9b9e9cdd4cdf87184
mobtone <tonenordstrom@hotmail.com>
2024-10-16 11:07:53 +0200

    Klass UserAuthentication

---
+++ UserAuthentication.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupparbeteFoodapplication
+{
+    public class UserAuthentication
+    {
+        private bool isLoggedIn;
+        private bool isAdmin;
+
+        public bool Login(string username, string password)
+        {
+            if (username == "admin" && password == "password")
+            {
+                isLoggedIn = true;
+                isAdmin = true;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Logout()
+        {
+            isLoggedIn = false;
+            isAdmin = false;
+        }
+
+        public bool IsAdmin()
+        {
+            return isAdmin;
+        }
+    }
+}
+
+
+        
+    
