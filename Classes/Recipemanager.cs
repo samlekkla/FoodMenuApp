56a4ceae068e997f4d2bdef4b569a9b3aac3e21b
harithasarangam <haritha@HARITHA>
2024-10-16 19:23:29 +0200

    recipemanager class wasscreated

---
+++ Classes/Recipemanager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace GrupparbeteFoodapplication.Classes
+{
+    public class Recipemanager
+    {
+        private const string filePath = "recipes.json";
+
+        public class Recipe
+        {
+
+        }
+
+        public List<Recipe> LoadRecipes()
+        {
+            if (!File.Exists(filePath))
+                return new List<Recipe>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<Recipe>>(json) ?? new List<Recipe>();
+        }
+
+        public void SaveRecipes(List<Recipe> recipes)
+        {
+            var json = JsonConvert.SerializeObject(recipes, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
