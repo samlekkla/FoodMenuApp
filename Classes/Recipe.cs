a55b3c316ac6ca521784ee8524b52dc4fae7e9a8
harithasarangam <haritha@HARITHA>
2024-10-16 11:58:12 +0200

    haritha made changes

---
+++ Classes/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GrupparbeteFoodapplication.Classes
+{
+   
+        public class Recipe
+        {
+            public string Type { get; set; }
+            public string Title { get; set; }
+            public string Beskrivning { get; set; }
+
+
+
+        }
+    
+}
