73e1f84f05aeccca38c3b679d86c5ea33b4ff7da
ebru mermer <ebrumermer@live.se>
2024-10-22 10:30:05 +0200

    New class for admin login

---
+++ Admin.cs
@@ -0,0 +1,29 @@
+﻿using GrupparbeteFoodapplication.Classes;
+
+namespace GrupparbeteFoodapplication
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            //Inloggningsknapp med textbox för användarnamn och lösenord
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+
+            // Skapa en instans av Adminlogin-klassen
+            AdminLogin adminLogin = new AdminLogin();
+
+            // Validera inloggning med användarnamn och lösenord
+            string loginResult = adminLogin.ValidateAdmin(username, password);
+
+            // Visa resultatet för användaren med en MessageBox
+            MessageBox.Show(loginResult, "Inloggningsstatus", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+}
